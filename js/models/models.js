ce7a241c3626c2765c06d61b101c50f97afb54b9
xingyuncaoxyz <54649637+xingyuncaoxyz@users.noreply.github.com>
2019-08-31 10:13:27 +0800

    Update models.js

--- js/models/models.js
+++ js/models/models.js
@@ -7,23 +7,23 @@ var URL = Backbone.Model.extend({
 
   validate: function(attrs) {
     var regex = /[$^()\[\]{}|<>]/g
-    $('.successMsg').text('') // Delete success msg
+    $('.successMsg').text('') // 删除成功消息
     if (attrs.name.length <= 1) {
-      $('#ErrorName').text(' *The name must be longer than one character. ')
-      return 'The name must be longer than one character'
+      $('#ErrorName').text(' *名称必须长于一个字符. ')
+      return '名称必须长于一个字符'
     }
     if (regex.test(attrs.name)) {
-      $('#ErrorName').text(' *No $^()[]{}|<> is allowed')
-      return 'No $^()[]{}|<> is allowed'
+      $('#ErrorName').text(' *这些字符 $^()[]{}|<> 不允许用')
+      return '这些字符 $^()[]{}|<> 不允许用'
     }
     $('#ErrorName').text('')
     if (attrs.url.length <= 1) {
-      $('#errorUrl').text(' *The URL must be longer than one character.')
-      return 'The URL must be longer than one character'
+      $('#errorUrl').text(' *网址URL必须长于一个字符.')
+      return '网址URL必须长于一个字符'
     }
     if (regex.test(attrs.url)) {
-      $('#errorUrl').text(' *No $^()[]{}|<> is allowed')
-      return 'No $^()[]{}|<> is allowed'
+      $('#errorUrl').text(' *这些字符 $^()[]{}|<> 不允许用')
+      return '这些字符 $^()[]{}|<> is allowed'
     }
     $('#errorUrl').text('')
     $('#errorSection').text('')
@@ -37,8 +37,8 @@ var section = Backbone.Model.extend({
   validate: function(attrs) {
     if (attrs.name.length <= 1) {
       $('#successMsgSection').text('')
-      $('#SectionNameError').text(' *The section name must be longer than one character. ')
-      return 'The section name must be longer than one character'
+      $('#SectionNameError').text(' *分类名称必须长于一个字符. ')
+      return '分类名称必须长于一个字符'
     }
     $('#SectionNameError').text('')
   }
